9bae9110a31e5db948a0a657dc5bd7286c24046a
shahmir811 <shahmirkj@gmail.com>
2020-11-20 11:17:23 +0500

    Set landing page

--- resources/js/src/pages/Landing/Landing.vue
+++ resources/js/src/pages/Landing/Landing.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    <h1>This is my landing page</h1>
-  </div>
+<div>
+  <div class='landing-page-wrapping-div'>
+    </div>
+      <div class="landing-page-centered-div">
+        <h1>Welcome to Cobbler Cartel CMS</h1>
+      </div>
+</div>
+  
 </template>
 
 <script>
@@ -11,5 +16,31 @@ export default {
 </script>
 
 <style lang="scss">
-  
+
+@import "../../styles/styles.scss"; 
+
+.landing-page-wrapping-div {
+  background-image: url("./leather-shoe.png");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover; 
+  opacity: 0.5;
+  width: 102%;
+  height: 92vh;
+  padding: 0px;
+  margin: 0px;
+  margin-left: -15px;
+  // position: relative;
+}
+
+.landing-page-centered-div {
+  position: absolute;
+  opacity: 1 !important;
+  background-color: $BROWN-12;
+  color: $GREY-2;
+  padding: 50px;
+  top: 35%;
+  left: 30%;
+}
+
 </style>
